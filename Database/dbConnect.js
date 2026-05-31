415a5779621cb93b9f52a5c03e064668316e64d9
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2026-05-31 16:13:30 +0300

    modifeid database

--- Database/dbConnect.js
+++ Database/dbConnect.js
@@ -2,9 +2,19 @@ import mongoose from "mongoose";
 
 export const dbConnect = async () => {
     try {
-        const mongodb_url = process.env.MONGODB_URL;
-        await mongoose.connect(mongodb_url);
-        console.log("Connected to MongoDB Atlas");
+
+        // const DB_HOST = "mongo";
+        // const DB_PORT =  27017;
+        // const DB_USER = "root";
+        // const DB_PASSWORD = "example";
+        // const DB_NAME = "ecommerce";
+
+        // const uri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+        const uri = process.env.MONGODB_URL;
+
+        await mongoose.connect(uri);
+
+        console.log("Connected to MongoDB successfully!");   
 
     } catch (error) {
         console.log(error);
